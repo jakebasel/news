a49f20d8be5175ebc4506e90e9ff4f653fab1993
Jake <jake@Jakes-MacBook-Air.local>
2021-08-29 12:01:58 -0700

    with latest updates

--- articles/urls.py
+++ articles/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import ArticleDeleteView, ArticleListView, ArticleCreateView, ArticleUpdateView, ArticleDetailView
 
 urlpatterns = [
-    path('', ArticleListView.as_view(), name='articles'),
+    path('', ArticleListView.as_view(), name='article_list'),
     path('<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
-    path('new/', ArticleCreateView.as_view(), name='create'),
-    path('<int:pk>/update', ArticleUpdateView.as_view(), name='update'),
-    path('<int:pk>/delete', ArticleDeleteView.as_view(), name='delete'),
+    path('new/', ArticleCreateView.as_view(), name='article_new'),
+    path('<int:pk>/update', ArticleUpdateView.as_view(), name='article_update'),
+    path('<int:pk>/delete', ArticleDeleteView.as_view(), name='article_delete'),
 ]
--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from environs import Env
+
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_d9alx986*^z-##m&8*3!7bnevql%4dl@@!)qxs7a&n#_rq4h_'
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', default=False)
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1",".herokuapp.com"]
 
@@ -40,6 +44,7 @@ INSTALLED_APPS = [
     
     # 3rd party
     'crispy_forms',
+    'whitenoise.runserver_nostatic',
 
     #local
     'accounts',
@@ -50,6 +55,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -82,10 +88,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.dj_db_url('DATABASE_URL')
 }
 
 
@@ -126,6 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRES = [str(BASE_DIR.joinpath('static'))]
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -138,3 +144,15 @@ LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+EMAIL_BACKEND = env.str(
+    'EMAIL_BACKEND',
+    default='django.core.mail.backends.smtp.EmailBackend')
+
+DEFAULT_FROM_EMAIL = env.str('DEFAULT_FROM_EMAIL')
+EMAIL_HOST = env.str('EMAIL_HOST')
+EMAIL_HOST_USER = env.str('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env.str('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
